ce4712ab198121a8529ddc640f2df24220110f0a
Alvaro <6666alvaro666@gmail.com>
2020-04-28 17:57:08 -0500

    delete item and delete all items

--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@
 const car = document.getElementById('carrito');
 const course = document.getElementById('lista-cursos');
 const listCourse = document.querySelector('#lista-carrito tbody');
+const emptyCarBtn = document.getElementById('vaciar-carrito');
 
 //listener 
 EventListener();
 
 
 function EventListener(){
-
+    //add car
     course.addEventListener('click',addCar);
+    //delete car 
+    car.addEventListener('click',deleteCar);
+    //empty car 
+    emptyCarBtn.addEventListener('click',emptyCar);
 }
 
 //funciones 
@@ -37,12 +42,11 @@ function readDataCouse (course){
 }
 
 //insert Car 
-
 function insertCar(course){
     const row =  document.createElement('tr');
     row.innerHTML= `
         <td>
-            <img src="${course.imagen}"/>
+            <img src="${course.imagen}" width=100/>
         </td>
         <td>
            ${course.title}
@@ -57,6 +61,20 @@ function insertCar(course){
     listCourse.appendChild(row);
 }
 
+//delete car 
 
+function deleteCar(e){
+    e.preventDefault();
+    
+    let couse;
+    if (e.target.classList.contains('borrar-curso')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
 
-
+//empty car 
+function emptyCar(){
+    while(listCourse.firstChild){
+        listCourse.removeChild(listCourse.firstChild);
+    }
+}
